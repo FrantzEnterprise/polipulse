73b6aa0ee7834f0207f85ed236c39aa84a01591e
Robert Frantz <your@email.comrobert@frantzenterprise.com>
2025-04-12 02:29:01 -0700

    Initial commit of PoliPulse from correct folder

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PoliPulseApp from './PoliPulseApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PoliPulseApp />
+  </React.StrictMode>
+)
