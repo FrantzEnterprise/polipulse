9bc143d256e6d2bc524af9177959c52e0581c202
Robert Frantz <your@email.comrobert@frantzenterprise.com>
2025-04-12 13:26:37 -0700

    Fix: added default export to PoliPulseApp.jsx

--- src/PoliPulseApp.jsx
+++ src/PoliPulseApp.jsx
@@ -1,1 +1,12 @@
 import React from "react";
+
+function PoliPulseApp() {
+  return (
+    <div>
+      {/* Your app content here */}
+      <h1>PoliPulse is Live!</h1>
+    </div>
+  );
+}
+
+export default PoliPulseApp;
