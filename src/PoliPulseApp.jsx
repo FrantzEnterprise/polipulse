345e91a1a114b56373947b1a473084b9da7011b0
Robert Frantz <your@email.comrobert@frantzenterprise.com>
2025-04-12 14:00:19 -0700

    Force rebuild: made visible update to component

--- src/PoliPulseApp.jsx
+++ src/PoliPulseApp.jsx
@@ -4,7 +4,8 @@ function PoliPulseApp() {
   return (
     <div>
       {/* Your app content here */}
-      <h1>PoliPulse is Live!</h1>
+      <h1>PoliPulse is Finally Visible!</h1>
+      <p>This should show up on the live site. 🎉</p>
     </div>
   );
 }
